a540874cd89f6e097929a11c21cfe54feb6db999
albiyandc <albiyandc65@gmail.com>
2025-06-02 14:51:45 +0700

    fix migrasi gedungs

--- database/migrations/2025_05_22_103750_gedungs_table.php
+++ database/migrations/2025_05_22_103750_gedungs_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('gedungs', function (Blueprint $table) {
             $table->id('gedung_id');
             $table->string('nama_gedung');
+            $table->string('lokasi')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->integer('kapasitas_total')->default(0);
             $table->timestamps();
         });
 
